b7a739ff5efc05dc13a6f7a60fe2adb6ce4bb0f6
outfrost <kotlet.bahn@gmail.com>
2018-03-10 01:31:16 +0100

    Add a few exercises for 1st lab class

---
+++ lab1/04_num-x-size.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+i=1;
+
+for name in ${1:-.}/*
+do
+	du -0bs --apparent-size "$name"
+done | sort -z -k 1 -n | sed -z 's/^[0-9]*\t//' | \
+while IFS= read -r -d '' name
+do
+	NUMBER=$i rename 's/$/.$ENV{NUMBER}/' $name
+	((i++))
+done
