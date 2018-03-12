6f47166049b62fd8c4e6162b6354f210bd38d6b0
outfrost <kotlet.bahn@gmail.com>
2018-03-12 10:45:33 +0100

    Rework exercises 02 and 08 at lab 1

--- lab1/02_touch-list.sh
+++ lab1/02_touch-list.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
-while read filename
-do
-	touch ${1:-.}/$filename
-done < $2
+if [ "$1" ] && [ -d "$1" ] && [ -r "$1" ] && [ -x "$1" ]
+then
+	if [ "$2" ] && [ -f "$2" ] && [ -r "$2" ]
+	then
+		while IFS= read -r filename
+		do
+			if [ ! -e "$1/$filename" ]
+			then
+				touch "$1/$filename"
+			fi
+		done < $2
+	else
+		echo "$2 (arg2) is not a file or is not accessible."
+		echo "Usage: $0 <target_directory> <list_file>"
+	fi
+else
+	echo "$1 (arg1) is not a directory or is not accessible."
+	echo "Usage: $0 <target_directory> <list_file>"
+fi
--- lab1/08_list-r.sh
+++ lab1/08_list-r.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-path=${1:-.}
-depth=${2:-0}
-originalpath=${3:-$path}
-
-for name in $path/*
-do
-	if [ -d $name ]
-	then
-		if [ $depth -lt 2 ]
+if [ "$1" ] && [ -d "$1" ] && [ -r "$1" ] && [ -x "$1" ]
+then
+	path="$1"
+	depth="${2:-0}"
+	originalpath=$(echo "${3:-$path}" | sed -e 's/[]\/$*.^[]/\\&/g')
+	
+	for name in "$path"/*
+	do
+		if [ -d "$name" ]
 		then
-			$0 $name $(( $depth + 1 )) $path
+			if [ "$depth" -lt "2" ]
+			then
+				$0 "$name" $(( $depth + 1 )) "$path"
+			fi
+		else
+			echo "$name" | sed "s/$originalpath\///"
 		fi
-	else
-		echo $name | sed "s/$originalpath\///"
-	fi
-done
+	done
+else
+	echo "$1 (arg1) is not a directory or is not accessible."
+	echo "Usage: $0 <directory>"
+fi
