8503ad8acae4bf6fa104edaa6cd58485c448fd46
outfrost <kotlet.bahn@gmail.com>
2018-04-23 10:30:07 +0200

    Finalise work on 2nd lab

--- lab2/02_traverse.sh
+++ lab2/02_traverse.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-./01_mkfiles.sh
-
-cd lab2/
-abspath_plik1=$(realpath -s a/b/c/plik1)
-
-echo "Links pointing to lab2/a/b/c/plik1:"
-for link in e/*
-do
-	if [ "$(readlink -f $link)" = "$abspath_plik1" ]
-	then
-		realpath -s $link
-	fi
-done
+if [ -x 01_mkfiles.sh ]
+then
+	./01_mkfiles.sh
+	
+	cd lab2/
+	abspath_plik1="$(realpath -s a/b/c/plik1)"
+	
+	echo "Links pointing to lab2/a/b/c/plik1:"
+	for link in e/*
+	do
+		if [ "$(readlink -f $link)" = "$abspath_plik1" ]
+		then
+			realpath -s "$link"
+		fi
+	done
+	
+	mkdir -p f
+	cp -P e/link_plikB_bez f/
+	cp "$(readlink -f e/link_plikA)" f/
+else
+	echo "Required dependency not found: 01_mkfiles.sh"
+	echo "Make sure 01_mkfiles.sh exists and is executable."
+fi
